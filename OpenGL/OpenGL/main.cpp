6abc45058519399d3941b65a93f60eb1028b03fd
JJ <jonathansimonjones@gmail.com>
2014-01-15 17:35:32 +0000

    Added basic framework, renders 2D white triangle

---
+++ OpenGL/OpenGL/main.cpp
@@ -0,0 +1,165 @@
+//#define GLEW_STATIC
+#include <GL\glew.h>
+//#include <SFML\OpenGL.hpp>
+#include <SFML\Graphics.hpp>
+//#include <SFML\Window.hpp>
+#include <Windows.h>
+
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+// Prototypes
+std::string readTextFromFile(std::string nameOfFile);
+void checkShaderForErrors(GLuint shader);
+void createShaderProgram(GLuint &shaderProgram_);
+
+int main()
+{
+	// Create a window, set size, give name, set style
+	sf::RenderWindow window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Close);
+
+	// OpenGL vars
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// Create a circle shape
+	sf::CircleShape shape(100.0f);
+	shape.setFillColor(sf::Color::Green);
+
+	// Set Verts for triangle
+	float vertices[] = {
+		 0.0f,  0.5f,	// Vertex 1 (X, Y)
+		 0.5f, -0.5f,	// Vertex 2 (X, Y)
+		-0.5f, -0.5f	// Vertex 3 (X, Y)
+	};
+
+	// Setup vertex buffer
+	// This is used to store the vert info that is to be passed to the graphics card
+	GLuint vertexBuffer;				// Reference to memory where buffer is
+	glGenBuffers(1, &vertexBuffer);		// We only want one buffer
+	
+	printf("%u\n", vertexBuffer);		// Print out the number of buffers
+
+	// Make buffer active
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);	// Make vertexBuffer the active array buffer
+													// Now that it is active we can copy the vertex data to it
+
+	// Copy vertex data to the active array buffer (currently vertexBuffer)
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		// glBufferData(the array to deal with, size in bytes, pointer to data, usage of vertex data)
+
+	// Create shader program
+	GLuint shaderProgram;
+	createShaderProgram(shaderProgram);
+
+	// Set shader to graphcics pipeline
+	glUseProgram(shaderProgram);
+
+	// Set up vertex array object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+
+	// Bing the vertex array object
+	glBindVertexArray(vao);
+	
+	// Get reference to position in vertex shader
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+
+	// Specify the format of the attribute
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	// Enable vertex shader attribute
+	glEnableVertexAttribArray(posAttrib);
+
+	// While window open
+	while (window.isOpen())
+	{
+		// Handle messages
+		sf::Event windowEvent;
+		while (window.pollEvent(windowEvent))
+		{
+			// Handle events
+			switch(windowEvent.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+
+			case sf::Event::KeyPressed:
+				if(windowEvent.key.code == sf::Keyboard::Escape)
+					window.close();
+			}
+		}
+
+		window.clear();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
+
+std::string readTextFromFile(std::string nameOfFile)
+{
+	// !Add error checking to see if file open correctly
+	std::ifstream vertexShaderSource(nameOfFile);
+	std::stringstream vertexShaderText;
+	vertexShaderText << vertexShaderSource.rdbuf();
+	return vertexShaderText.str();
+}
+
+void checkShaderForErrors(GLuint shader)
+{
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+
+	if(status != GL_TRUE)
+	{
+		char buffer[512];
+		glGetShaderInfoLog(shader, 512, NULL, buffer);
+		std::cout << "Failed to compile shader, reasons to follow:" << std::endl;
+		std::cout << buffer << std::endl;
+		std::cin >> buffer;
+	}
+}
+
+void createShaderProgram(GLuint &shaderProgram_)
+{
+	// Get vertex buffer 
+	std::string vertexShaderText = readTextFromFile("outputVertex.vert");
+	const char *vertexShaderData = vertexShaderText.c_str();
+
+	// Create vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	// Load the data into the shader
+	glShaderSource(vertexShader, 1, &vertexShaderData, NULL);
+
+	// Compile the vertex shader
+	glCompileShader(vertexShader);
+
+	// Get fragment buffer
+	std::string fragmentShaderText = readTextFromFile("colourEverythingWhite.frag");
+	const char *fragmentShaderData = fragmentShaderText.c_str();
+
+	// Create fragment shader 
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	
+	// Load the data into the shader
+	glShaderSource(fragmentShader,1, &fragmentShaderData, NULL);
+
+	// Compile the fragment shader
+	glCompileShader(fragmentShader);
+
+	// Create shader program 
+	shaderProgram_ = glCreateProgram();
+	glAttachShader(shaderProgram_, vertexShader);
+	glAttachShader(shaderProgram_, fragmentShader);
+
+	// Bing fragment shader output to buffer
+	glBindFragDataLocation(shaderProgram_, 0, "outColor");
+
+	// Finished construction of shader by linking it
+	glLinkProgram(shaderProgram_);
+}
