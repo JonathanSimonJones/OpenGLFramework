628e83413559597cac16babf3b8be2b357d7fdf0
JJ <jonathansimonjones@gmail.com>
2014-01-19 16:59:00 +0000

    Commit before implementing seperate vertex list for bounding box

--- Game/OpenGL/OpenGL/VertexShaders/color3D.vert
+++ Game/OpenGL/OpenGL/VertexShaders/color3D.vert
@@ -3,16 +3,17 @@
 in vec3 position;
 in vec3 color;
 
-out vec3 Color;
+out vec4 Color;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
 
 uniform vec3 overrideColor;
+uniform float Alpha;
 
 void main()
 {
-	Color = overrideColor * color;
+	Color = vec4(color, Alpha);
 	gl_Position = proj * view * model * vec4(position, 1.0);
 }
--- Source/Sources/main.cpp
+++ Source/Sources/main.cpp
@@ -25,10 +25,19 @@
 #include <sstream>
 #include <iostream>
 
+struct AABB
+{
+	glm::vec3 center_position;
+	glm::vec3 radius;
+};
+
 // Prototypes
 std::string readTextFromFile(std::string nameOfFile);
 void checkShaderForErrors(GLuint shader);
 void createShaderProgram(GLuint &shaderProgram_);
+inline glm::fquat AngularVelocityToSpin(glm::fquat orientation, glm::vec3 angularVelocity);
+bool testAABBAABB(const AABB &a, const AABB &b);
+
 
 // Main
 int main()
@@ -43,8 +52,14 @@ int main()
 	glewExperimental = GL_TRUE;
 	glewInit();
 
+	// Get openGL version
+	printf("OpenGL version supported by this platform (%s): \n", glGetString(GL_VERSION));
+
 	// Enable depth testing
 	glEnable(GL_DEPTH_TEST);
+
+	// Enable alpha blending 
+	//glEnable(GL_BLEND);
 
 	// Set up vertex array object
 	GLuint vao;
@@ -53,17 +68,9 @@ int main()
 	// Bing the vertex array object
 	glBindVertexArray(vao);
 
-	/*
-	float vertices[] = {
-	  // X      Y     Z     R     G     B    
-		-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 
-		 0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-		 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 
-		-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f
-	};
-	*/
-
 	GLfloat vertices[] = {
+
+		// First cube
 		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 
 		 0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 
 		 0.5f,  0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 
@@ -105,6 +112,57 @@ int main()
 		 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 0.0f,
 		-0.5f,  0.5f,  0.5f, 1.0f, 0.0f, 1.0f,
 		-0.5f,  0.5f, -0.5f, 0.0f, 1.0f, 1.0f,
+
+		// Second cube
+		-0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f, 
+		 0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f, 
+		 0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f, 
+		 0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f, 
+		-0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f, 
+		-0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f, 
+
+		-0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f, 
+		 0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+
+		-0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+
+		 0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+
+		-0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f, -0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f, -0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+
+		-0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		 0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f,  0.5f,  0.5f, 0.3f, 0.3f, 0.3f,
+		-0.5f,  0.5f, -0.5f, 0.3f, 0.3f, 0.3f,
+
+		// Floor
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.5f, 
+		 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.5f,
+		 0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 0.5f,
+		 0.5f,  0.5f, 0.0f, 0.5f, 0.0f, 0.0f, 
+		-0.5f,  0.5f, 0.0f, 0.5f, 0.0f, 0.0f, 
+		-0.5f, -0.5f, 0.0f, 0.5f, 0.0f, 0.0f, 
 	};
 
 	// Setup vertex buffer
@@ -119,6 +177,14 @@ int main()
 	// Copy vertex data to the active array buffer (currently vertexBuffer)
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		// glBufferData(the array to deal with, size in bytes, pointer to data, usage of vertex data)
 
+	float boundingBoxVertices[] = {
+	  // X      Y     Z     R     G     B    
+		-0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 
+		 0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 
+		-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f
+	};
+	
 	// Set up the elements
 	GLuint elements[] = { 
 		0, 1, 2,
@@ -154,14 +220,24 @@ int main()
 	glEnableVertexAttribArray(posAttrib);
 	glEnableVertexAttribArray(colAttrib);
 
+	// Box 1
 	// 2D rotation
 	glm::mat4 model;
 	glm::fquat orientation(1.0f, 0.0f, 0.0f, 0.0f);
 	glm::vec3 position(0.0f, 0.0f, 0.0f);
-	position += glm::vec3(-5.0f, 0.0f, 0.0f);
+
+	const float fps = 200.0f;
+	const float dt = 1 / fps;
+	glm::vec3 velocity(0.1f, 0.0f, 0.0f);
+	glm::vec3 gravity(0.0f, 0.0f, -1.0f);
+	glm::vec3 angularVelocity(-0.1f, 0.0f, 0.1f);
+
+	position += glm::vec3(0.0f, 0.0f, 0.0f);
 	model = glm::translate(model, position);
 
 	glm::mat4 ident;
+
+	AABB BBB1;
 
 	// Set up uniform attribute 
 	GLint uniModel = glGetUniformLocation(shaderProgram, "model");
@@ -169,7 +245,7 @@ int main()
 
 	// Create view matrix
 	glm::mat4 view = glm::lookAt(			// Looks at specifies XY plane as ground, Z axis is up
-		glm::vec3(0.0f, -10.0f, 2.0f),		// Position
+		glm::vec3(0.0f, -5.0f, 2.0f),		// Position
 		glm::vec3(0.0f, 0.0f, 0.0f),		// Where to look on the screen, forward vector
 		glm::vec3(0.0f, 0.0f, 1.0f)			// The up axis
 		);
@@ -187,33 +263,104 @@ int main()
 	GLint uniColor = glGetUniformLocation(shaderProgram, "overrideColor");
 	glUniform3f(uniColor, 1.0f, 1.0f, 1.0f);
 
-	const float fps = 200.0f;
-	const float dt = 1 / fps;
-	glm::vec3 velocity(0.1f, 0.0f, 0.0f);
-	glm::vec3 gravity(0.0f, 0.0f, -1.0f);
+	// Set up unifrom alpha
+	GLint uniAlpha = glGetUniformLocation(shaderProgram, "Alpha");
+	glUniform1f(uniAlpha, 1.0f);
 
-	float rotation = 0.0f;
+	// Box 2
+	glm::vec3 box2Pos(3.0f, 0.0f, 0.5f);
+	float movSpeed = 4.0f;
+	AABB BBB2;
 
 	// While window open
 	while (window.isOpen())
 	{
-
-		//velocity += gravity * dt;
-		position += velocity * dt;
-		model = glm::translate(ident, position);
-		model = glm::rotate( model, rotation += 0.1f, glm::vec3(0.0f, 0.0f, 1.0f) );
-		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+		
 
 		// Set clear colour
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		// Clear back buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-		// Draw cube
-		glDrawArrays(GL_TRIANGLES, 0, 36);
 
+		// Box 1
+			// Advance linearly
+			//velocity += gravity * dt;
+			//position += velocity * dt;
+			model = glm::translate(ident, position);
+
+			// Calculate rotation
+			glm::fquat spin = AngularVelocityToSpin(orientation, angularVelocity);
+			orientation = orientation + spin * dt;
+			orientation = glm::normalize(orientation);
+			model = glm::rotate( model, glm::angle(orientation), glm::axis(orientation) );
+		
+			// Upload variables to graphics mem
+			glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+			glUniform1f(uniAlpha, 1.0f);
+
+			// Draw cube
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// Box 2
+			BBB2.center_position = box2Pos;
+			BBB2.radius = glm::vec3(0.5f, 0.5f, 0.5f);
+
+			// Calculate position
+			model = glm::translate(ident, box2Pos);
+
+			// Upload variables to graphics mem
+			glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+			glUniform1f(uniAlpha, 1.0f);
+
+			// Draw cube
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// AABB Box 1
+			// Enable alpha blending 
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR);
+
+			// Calculate position
+			model = glm::translate(ident, position);
+			model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+
+			BBB1.center_position = position;
+			BBB1.radius = glm::vec3(0.5f * 1.5f, 0.5f * 1.5f, 0.5f * 1.5f);
+		
+			// Upload variables to graphics mem
+			glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+			glUniform1f(uniAlpha, 0.5f);
+
+			// Draw bounding box
+			glDrawArrays(GL_TRIANGLES, 36, 36);
+
+			// Disable alpha blending
+			glDisable(GL_BLEND);
+
+		// Floor
+			// Calculate position
+			model = glm::translate(ident, glm::vec3(0.0f, 0.0f, -0.5f));
+			model = glm::scale(model, glm::vec3(10.0f, 10.0f, 1.0f));
+		
+			// Upload variables to graphics mem
+			glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+			glUniform1f(uniAlpha, 1.0f);
+
+			// Draw bounding box
+			glDrawArrays(GL_TRIANGLES, 72, 6);
+
+		
 		// Display back buffer
 		window.display();
+
+		if(testAABBAABB(BBB1, BBB2))
+		{
+			std::cout << "Boxes colliding" << std::endl;
+		}
+		else
+		{
+			std::cout << "No collision" << std::endl;
+		}
 
 		// Handle messages
 		sf::Event windowEvent;
@@ -226,8 +373,32 @@ int main()
 				window.close();
 
 			case sf::Event::KeyPressed:
-				if(windowEvent.key.code == sf::Keyboard::Escape)
-					window.close();
+				{
+					if(windowEvent.key.code == sf::Keyboard::Escape)
+					{
+						window.close();
+					}
+
+					if(windowEvent.key.code == sf::Keyboard::Up)
+					{
+						box2Pos.y += movSpeed * dt;
+					}
+
+					if(windowEvent.key.code == sf::Keyboard::Down)
+					{
+						box2Pos.y -= movSpeed * dt;
+					}
+
+					if(windowEvent.key.code == sf::Keyboard::Left)
+					{
+						box2Pos.x -= movSpeed * dt;
+					}
+
+					if(windowEvent.key.code == sf::Keyboard::Right)
+					{
+						box2Pos.x += movSpeed * dt;
+					}
+				}
 			}
 		}
 	}
@@ -321,4 +492,13 @@ inline glm::fquat AngularVelocityToSpin(glm::fquat orientation, glm::vec3 angula
 	const float z = angularVelocity.z;
 
 	return 0.5f * glm::fquat(0, x, y, z) * orientation;
+}
+
+bool testAABBAABB(const AABB &a, const AABB &b)
+{
+	if(glm::abs(a.center_position.x - b.center_position.x) > (a.radius.x + b.radius.x)) return false;
+	if(glm::abs(a.center_position.y - b.center_position.y) > (a.radius.y + b.radius.y))	return false;
+	if(glm::abs(a.center_position.z - b.center_position.z) > (a.radius.z + b.radius.z)) return false;
+				
+	return true;
 }
